b0a1baa82d0d7e1be3f7202b6a5ada9038ea877b
andrade-lopes <jlopes@byupathway.edu>
2025-09-06 21:50:15 -0100

    Exercise 2 added

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,67 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // Determine the letter grade
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign (+, -, or none)
+        string sign = "";
+        int lastDigit = grade % 10;
+
+        if (letter != "A" && letter != "F") // A+ doesn't exist, F has no sign
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && lastDigit < 3)
+        {
+            sign = "-"; // Only A- exists
+        }
+
+        // Print the letter grade with the sign
+        Console.WriteLine("Your letter grade is: " + letter + sign);
+
+        // Determine pass/fail
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Keep trying next time.");
+        }
     }
 }
