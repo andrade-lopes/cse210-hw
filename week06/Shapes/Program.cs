f47ab407ac547a496459403741e305a088f1ee4d
andrade-lopes <jlopes@byupathway.edu>
2025-10-07 21:13:45 -0100

    Week6 Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        // Create list of shapes (polymorphism in action)
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Iterate and display the areas and colors
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00}\n");
+        }
     }
 }
