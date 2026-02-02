d08e45b9781cdc5f13877c0ef13da83d12a82dc1
andrade-lopes <jlopes@byupathway.edu>
2026-02-02 21:09:36 -0100

    Assignment GetSummary () tested

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        // Test base class
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
     }
 }
