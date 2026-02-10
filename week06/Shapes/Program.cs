f081a803f6ae1e84a20e98ea915b060a9c1e8e68
andrade-lopes <jlopes@byupathway.edu>
2026-02-10 21:13:24 -0100

    List of shapes created

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        // Create list of shapes (polymorphism in action)
+        List<Shape> shapes = new List<Shape>();
+
+        
     }
 }
