120551eb9300e6039e79cb3835517d3387326b07
andrade-lopes <jlopes@byupathway.edu>
2026-02-10 21:28:17 -0100

    Square class code tested

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -9,6 +9,15 @@ class Program
         // Create list of shapes (polymorphism in action)
         List<Shape> shapes = new List<Shape>();
 
-        
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+
+
+        // Iterate and display the areas and colors
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00}\n");
+        }
     }
 }
